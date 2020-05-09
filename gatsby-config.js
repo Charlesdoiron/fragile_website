14cc9c86827c2c07c5d02dcb3d10c69ce9cce8c4
Charlesdoiron <doironcharles@gmail.com>
2020-05-09 21:48:18 +0200

    🔥

---
+++ gatsby-config.js
@@ -0,0 +1,74 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Starter`,
+    description: `Gatsby Starter`,
+    author: `Polychronis Papadakis`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`eczar`],
+        display: 'swap',
+      },
+    },
+    `gatsby-plugin-flow`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
+
+    {
+      resolve: 'gatsby-plugin-module-resolver',
+      options: {
+        root: './src',
+        aliases: {
+          components: './components',
+          containers: './containers',
+          images: './images',
+          state: './state',
+          styles: './styles',
+          utils: './utils',
+          static: {
+            root: './public',
+            alias: './static',
+          },
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter`,
+        short_name: `Gatsby Starter`,
+        start_url: `/`,
+        background_color: `black`,
+        theme_color: `black`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+      },
+    },
+
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        environment: process.env.CONTENTFUL_ENV,
+        host: 'cdn.contentful.com',
+      },
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div className=' container mx-auto '>
+      <h1 className='md:text-6xl m-0'>Societer</h1>
+      <p className='margin-0'>www.societer.co</p>
+      <p className=''>Ux / Ui / Front-end</p>
+      <h1 className='md:text-6xl'>1024</h1>
+      <p className='margin-0'>www.societer.co</p>
+      <p className=''>Ux / Ui / Front-end</p>
+      <h1 className='md:text-6xl'>Terre de Jeux</h1>
+      <p className='margin-0'>www.societer.co</p>
+      <p className=''>Ux / Ui / Front-end</p>
+      <h1 className='md:text-6xl'>YesDoc</h1>
+      <p className='margin-0'>www.societer.co</p>
+      <p className=''>Ux / Ui / Front-end</p>
+    </div>
+  );
+};
+
+export default IndexPage;
